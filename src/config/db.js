86be341081e1ac3cf60d59471c87a5a0e84645fa
alifankebima <alif.anke.bima@gmail.com>
2024-06-28 10:08:52 +0700

    added new db connection type

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,8 @@
 //PostgreSQL config
 const { Pool } = require("pg");
 const pool = new Pool({
+    connectionString: process.env.PGURL,
+    ssl: process.env.PGUSESSL && !!process.env.PGUSESSL,
     user: process.env.PGUSER,
     host: process.env.PGHOST,
     database: process.env.PGDATABASE,
